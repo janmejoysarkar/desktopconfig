77bc07b473cbb396dcb83f890903065c444a9bf9
janmejoy <janmejoysarker@gmail.com>
2024-04-26 12:05:09 +0530

    tmux config added

--- i3_config_backup/backup.sh
+++ i3_config_backup/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Made to rsync config files from the .config folder to the scriptss folder.
 rsync -rtv --progress --delete $HOME/.bashrc $HOME/scripts_git/i3_config_backup/bashrc
-rsync -rtv --progress --delete $HOME/.config/{i3,i3blocks,alacritty} $HOME/scripts_git/i3_config_backup/
+rsync -rtv --progress --delete $HOME/.config/{i3,i3blocks,alacritty,tmux} $HOME/scripts_git/i3_config_backup/
--- i3_config_backup/i3/autostart.sh
+++ i3_config_backup/i3/autostart.sh
@@ -2,7 +2,7 @@
 dunst &
 /home/janmejoyarch/scripts/dropbox.py start &
 nitrogen --restore &
-xset s off &
+xset s 540 &
 xset dpms 600 600 600 &
 picom &
 notify-send 'Hello' $USER &
